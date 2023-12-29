925d6a63d337f9f2765e0a70eb86d103cdd1981a
Piyush <piyushpatil4270@gmail.com>
2023-12-29 20:53:33 +0530

    added images

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -8,10 +8,10 @@ const Authentication = () => {
     <div className='w-full h-full flex flex-col justify-center    items-center xs:gap-[10px] md:gap-4 '>
      <div className='w-full flex xs:gap-[10px] md:gap-[15px] xss:pt-10 sm:pt-[30px] md:pt-[65px]  justify-center items-center'>
       <img src={Logo} className='xs:h-9 xs:w-50 sm:h-15 sm:w-50'/>
-       <span className=' font-medium xs:text-[40px] text-[50px] bg-gradient-to-r from-violet-900  to-pink-600 inline-block text-transparent bg-clip-text'>Doppler</span>
+       <span className=' font-medium xs:text-[40px] text-[50px] bg-gradient-to-r from-violet-900  to-pink-600 inline-block text-transparent bg-clip-text'>Vexa</span>
       </div>
       <div className='flex flex-col justify-center items-center'>
-       <span className='text-[#b6aeae] md:text-[25px] sm:text-[20px] text-[30]'>Welcome to Doppler</span>
+       <span className='text-[#b6aeae] md:text-[25px] sm:text-[20px] text-[30]'>Welcome to Vexa</span>
        </div>
        {openSignUp?(<SignUp setOpenSignUp={setOpenSignUp}/>):(<SignIn setOpenSignUp={setOpenSignUp}/>)}
     </div>
